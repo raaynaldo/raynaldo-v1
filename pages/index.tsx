bf05c96d298739e5be7d81f12d285b986780b960
Raynaldo Sutisna <raaynaldo@gmail.com>
2023-04-30 22:16:10 -0400

    feat: add tailwind primary color

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <header className='sticky top-0 bg-white/20 backdrop-blur-md'>
+      <header className='sticky top-0 bg-white/50 backdrop-blur-md'>
         <nav className='flex justify-center layout'>
           <ul className='flex items-center justify-between w-3/4 h-20 md:w-1/2'>
             <li>Home</li>
@@ -13,11 +13,11 @@ export default function Home() {
       </header>
 
       <main>
-        <section id='home' className='bg-red-300 '>
+        <section id='home' className='bg-white'>
           <div className='flex flex-col items-center justify-center h-screen-minus-div layout'>
-            <h3>Hello there 👋! My name is</h3>
-            <h1>Raynaldo Sutisna</h1>
-            <p className='text-center md:w-1/2'>
+            <h3>👋 Hello! My name is</h3>
+            <h1 className='text-primary-500'>Raynaldo Sutisna</h1>
+            <p className='mt-4 text-center md:w-1/2'>
               A passionate and skilled full-stack web developer building
               beautiful websites with love and a commitment to clean code.
             </p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,24 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        primary: {
+          50: '#f9e2e2',
+          100: '#f2c5c5',
+          200: '#eca7a7',
+          300: '#e68a8a',
+          400: '#df6d6d',
+          500: '#D95050',
+          600: '#cb2c2c',
+          700: '#a32323',
+          800: '#7a1a1a',
+          900: '#511212',
+          950: '#290909',
+        },
+        black: '#363032',
+        white: '#E7E7E7',
+      },
     },
   },
   plugins: [],
-}
+};
