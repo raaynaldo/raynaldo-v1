7d7b48bc3ceb91fea3517f3b5e96bd34350851bf
Raynaldo Sutisna <raaynaldo@gmail.com>
2023-05-01 00:45:57 -0400

    feat: add nunito font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,24 +9,22 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        primary: ['Nunito', ...defaultTheme.fontFamily.sans],
       },
       colors: {
         primary: {
-          50: '#f9e2e2',
-          100: '#f2c5c5',
-          200: '#eca7a7',
-          300: '#e68a8a',
-          400: '#df6d6d',
+          50: '#fdf3f3',
+          100: '#fbe5e5',
+          200: '#f8d0d0',
+          300: '#f2afaf',
+          400: '#e88181',
           500: '#D95050',
-          600: '#cb2c2c',
-          700: '#a32323',
-          800: '#7a1a1a',
-          900: '#511212',
-          950: '#290909',
+          600: '#c63c3c',
+          700: '#a62f2f',
+          800: '#8a2a2a',
+          900: '#732929',
+          950: '#3e1111',
         },
         black: '#363032',
         white: '#E7E7E7',
