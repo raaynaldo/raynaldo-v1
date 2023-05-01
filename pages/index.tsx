eda9485adc6c6db0c1d973efa601c7f626340a39
Raynaldo Sutisna <raaynaldo@gmail.com>
2023-05-01 00:46:15 -0400

    feat: hero done with social media

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
+import clsx from 'clsx';
+import { FaDev, FaGithubSquare, FaLinkedin } from 'react-icons/fa';
+
 export default function Home() {
   return (
     <>
       <header className='sticky top-0 bg-white/50 backdrop-blur-md'>
         <nav className='flex justify-center layout'>
-          <ul className='flex items-center justify-between w-3/4 h-20 md:w-1/2'>
+          <ul className='flex items-center justify-between w-3/4 h-20 sm:w-1/2'>
             <li>Home</li>
             <li>Experience</li>
             <li>Blog</li>
@@ -15,12 +18,57 @@ export default function Home() {
       <main>
         <section id='home' className='bg-white'>
           <div className='flex flex-col items-center justify-center h-screen-minus-div layout'>
-            <h3>👋 Hello! My name is</h3>
-            <h1 className='text-primary-500'>Raynaldo Sutisna</h1>
-            <p className='mt-4 text-center md:w-1/2'>
+            <h2 className='text-2xl sm:text-4xl 2xl:text-5xl'>
+              👋 Hello! My name is
+            </h2>
+            <h1
+              className={clsx(
+                'mt-2 mb-3 sm:mt-4 sm:mb-5',
+                'text-3xl sm:text-5xl 2xl:text-6xl'
+              )}
+            >
+              <span className='transition-colors bg-primary-300'>
+                Raynaldo Sutisna
+              </span>
+            </h1>
+            <p
+              className={clsx('text-center sm:w-3/5', 'sm:text-lg 2xl:text-xl')}
+            >
               A passionate and skilled full-stack web developer building
               beautiful websites with love and a commitment to clean code.
             </p>
+            <div className='flex gap-2 mt-2'>
+              <a
+                href='https://dev.to/raaynaldo'
+                className='cursor-ne-resize'
+                target='_blank'
+              >
+                <FaDev
+                  size={35}
+                  className='transition duration-150 ease-linear hover:text-primary-500'
+                />
+              </a>
+              <a
+                href='https://github.com/raaynaldo'
+                className='cursor-ne-resize'
+                target='_blank'
+              >
+                <FaGithubSquare
+                  size={35}
+                  className='transition duration-150 ease-linear hover:text-primary-500'
+                />
+              </a>
+              <a
+                href='https://www.linkedin.com/in/teofilusraynaldo/'
+                className='cursor-ne-resize'
+                target='_blank'
+              >
+                <FaLinkedin
+                  size={35}
+                  className='transition duration-150 ease-linear hover:text-primary-500'
+                />
+              </a>
+            </div>
           </div>
         </section>
 
