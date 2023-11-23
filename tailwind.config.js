71d762f6515d30b0dec3b458ec78ab16d23d7ebc
Raynaldo Sutisna <raaynaldo@gmail.com>
2023-11-22 21:21:17 -0500

    refactor: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,9 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 
 module.exports = {
   content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
